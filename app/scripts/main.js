75351f05ba2815f640f310245b248229f6199ef7
Emirates HTML <Emirates@anoops-MacBook-Pro.local>
2017-09-16 16:05:34 +0400

    added form action and result age styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,14 @@
 function outputUpdate(vol) {
 	document.querySelector('#ageCount').value = vol;
 }
+
+function calculateCamels() {
+	document.getElementById('camelCalcaulateContainer').classList.add("hidden");
+	document.getElementById('camelResults').classList.remove("hidden");
+}
+
+function recalculateCamels() {
+	document.getElementById('camelCalcaulateContainer').classList.remove("hidden");
+	document.getElementById('camelResults').classList.add("hidden");
+	document.getElementById("friendForm").reset();
+}
